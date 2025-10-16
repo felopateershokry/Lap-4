e75f185427dbf7de0d6547ca53c644c04fc1ed7c
ahmed-salama-R <ahmedibrahimss01230@gmail.com>
2025-10-16 17:29:27 +0300

    Product Class

---
+++ src/main/java/Product.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author PC
+ */
+public class Product {
+
+    private String productID;
+    private String productName;
+    private String manufacturerName;
+    private String supplierName;
+
+    private int quantity;
+
+    private float price;
+
+    public Product(String productID, String productName, String manufacturerName, String supplierName, int quantity, float price) {
+        this.productID = productID;
+        this.productName = productName;
+        this.manufacturerName = manufacturerName;
+        this.supplierName = supplierName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String lineRepresentation() {
+        return productID + "," + productName + "," + manufacturerName + ","
+                + supplierName + "," + quantity + "," + price;
+    }
+
+    public String getSearchKey() {
+        return productID;
+    }
+
+}
